1409d3932a3db960b92b1028bebd212bcae34cc1
joseadleer <josericardosilva166@gmail.com>
2023-09-08 18:25:34 -0300

    menu hamburguer

--- menu.js
+++ menu.js
@@ -1,4 +1,6 @@
 
+// Codigo  para  scroll
+
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem',window.scrollY > 100)
@@ -8,3 +10,16 @@ window.addEventListener("scroll", function(){
     let titulo = this.document.querySelector('#id-titulo')
     titulo.classList.toggle('rolagem2',window.scrollY > 120)
 })
+
+// Codigo para o menu responsivo
+
+function menuShow() {
+    let menuMobile = document.querySelector('.menu-mobile')
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "imagens/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "imagens/close_white_36dp.svg";
+    }
+}
